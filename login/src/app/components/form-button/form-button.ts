5fe4134e06442dcba194ca9e55b62a0fd800c4c4
Tala <talaalfaleh987@gmail.com>
2026-02-17 14:13:50 +0300

    feat: login button

---
+++ login/src/app/components/form-button/form-button.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-button',
+  imports: [],
+  templateUrl: './form-button.html',
+  styleUrl: './form-button.css',
+})
+export class FormButton {
+
+}
---
+++ login/src/app/pages/login/login.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FormButton } from "../../components/form-button/form-button";
+
+@Component({
+  selector: 'app-login',
+  imports: [FormButton],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login {
+
+}
