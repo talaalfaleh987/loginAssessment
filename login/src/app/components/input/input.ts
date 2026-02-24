55a35aa7cdd9d0b7837262e1860a02af1a4e023b
Tala <talaalfaleh987@gmail.com>
2026-02-24 10:20:51 +0300

    initial commit

---
+++ login/src/app/components/input/input.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input',
+  imports: [],
+  templateUrl: './input.html',
+})
+export class Input {}
--- login/src/app/enums/message.enum.ts
+++ login/src/app/enums/message.enum.ts
@@ -1,9 +1,3 @@
-export enum MessageType {
-  ERROR = 'error',
-  WARNING = 'warning',
-  INFO = 'info',
-  SUCCESS = 'success',
-}
 export enum MessageStyle {
   MESSAGE = 'w-full rounded-full border-2 font-semibold flex items-center justify-center px-6 py-3',
   ERROR = MESSAGE + ' border-error text-error bg-errorBg',
