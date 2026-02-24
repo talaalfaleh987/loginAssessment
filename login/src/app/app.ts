38947b02bc5b264e7a2ce9a0510bd9d06b11c237
Tala <talaalfaleh987@gmail.com>
2026-02-24 10:42:35 +0300

    feat: login ui

--- login/src/app/app.ts
+++ login/src/app/app.ts
@@ -1,7 +1,8 @@
 import { Component, signal } from '@angular/core';
+import { Login } from "./pages/login/login";
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Login],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
--- login/tailwind.config.js
+++ login/tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ['./src/**/*.{html,ts}'],
   theme: {
     extend: {
+      backgroundImage: {
+        'tahakom-navbar': 'linear-gradient(to right, #35142C, #612550, #497283)',
+        'tahakom-auth': 'linear-gradient(to bottom, #35142C, #612550, #497283, #31BFB7)',
+      },
       colors: {
         primary: '#612550',
         error: '#9A0B11',
