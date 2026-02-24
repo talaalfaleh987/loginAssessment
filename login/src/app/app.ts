1b40f27d827352f2606bf3a00ef4b492dbe24e84
Tala <talaalfaleh987@gmail.com>
2026-02-24 12:49:35 +0300

    feat: input

--- login/src/app/app.ts
+++ login/src/app/app.ts
@@ -1,10 +1,37 @@
 import { Component, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { InputStyle, InputType } from './enums/input.enum';
+import { ButtonStyle, ButtonType } from './enums/button.enum';
+import { MessageStyle } from './enums/message.enum';
+import { CustomButton } from './components/custom-button/custom-button';
+import { Input } from './components/input/input';
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [CustomButton, Input, ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('login');
+
+  //   submitted = signal(false);
+
+  submitted = false;
+  readonly InputType = InputType;
+  readonly ButtonType = ButtonType;
+  readonly ButtonStyle = ButtonStyle;
+  form = new FormGroup({
+    username: new FormControl('', { nonNullable: true, validators: [Validators.required] }),
+    password: new FormControl(0, {nonNullable: true,   validators: [Validators.required, Validators.min(8)],
+    }),
+    remember: new FormControl(false, { nonNullable: true, validators: [Validators.requiredTrue] }),
+  });
+
+  onSubmit(): void {
+    this.submitted = true;
+
+    if (this.form.invalid) return;
+
+    console.log(this.form.value);
+  }
 }
---
+++ login/src/app/enums/input.enum.ts
@@ -0,0 +1,11 @@
+export enum InputType {
+  TEXT = 'text',
+  PASSWORD = 'password',
+  EMAIL = 'email',
+  CHECKBOX = 'checkbox',
+  NUMBER = 'number',
+}
+export enum InputStyle {
+  MESSAGE = 'w-full rounded-md border-2  font-semibold flex items-center justify-center px-6 py-3',
+  ERROR = MESSAGE + ' border-error',
+}
