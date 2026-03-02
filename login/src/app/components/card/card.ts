fc35202e834b4928c4f5d3679bc40b46eeb4694a
Tala <talaalfaleh987@gmail.com>
2026-03-02 20:22:18 +0300

    fix: home ui

--- login/src/app/components/card/card.ts
+++ login/src/app/components/card/card.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   imports: [],
   templateUrl: './card.html',
 })
-export class Card {}
+export class Card {
+  hasBorder = input<boolean>(true);
+}
