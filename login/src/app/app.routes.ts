02e19e5438144d58c95a11ba05ca2d2ca99bf1da
Tala <talaalfaleh987@gmail.com>
2026-03-04 11:34:23 +0300

    feat: login validation

--- login/src/app/app.routes.ts
+++ login/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Home } from './pages/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: Login },
+  { path: 'home', component: Home },
+];
--- login/src/app/pages/login/login.ts
+++ login/src/app/pages/login/login.ts
@@ -1,8 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { InputType, ValidatorType } from '../../enums/input.enum';
+import { ButtonStyle, ButtonType } from '../../enums/button.enum';
+import { MessageStyle } from '../../enums/message.enum';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { InputErrorMessage } from '../../models/input-error-message';
+import { Message } from '../../components/message/message';
+import { CustomInput } from '../../components/input/input';
+import { CustomButton } from '../../components/custom-button/custom-button';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CustomButton, CustomInput, ReactiveFormsModule, Message],
   templateUrl: './login.html',
 })
-export class Login {}
+export class Login {
+  submitted = signal(false);
+
+  router = inject(Router);
+
+  readonly InputType = InputType;
+  readonly ButtonType = ButtonType;
+  readonly ButtonStyle = ButtonStyle;
+  readonly MessageStyle = MessageStyle;
+
+  form = new FormGroup({
+    username: new FormControl('', {
+      validators: [Validators.required, Validators.pattern('^[0-9]+$')],
+    }),
+    password: new FormControl('', { validators: [Validators.required, Validators.minLength(8)] }),
+    rememberMe: new FormControl(false),
+  });
+
+  usernameErrors: InputErrorMessage[] = [
+    { message: 'Username is required', types: [ValidatorType.required] },
+  ];
+
+  passwordErrors: InputErrorMessage[] = [
+    { message: 'Password is required', types: [ValidatorType.required] },
+    { message: 'Password must be at least 8 characters', types: [ValidatorType.minlength] },
+  ];
+
+  onSubmit(): void {
+    this.submitted.set(true);
+    this.form.markAllAsTouched();
+    if (this.form.invalid) return;
+
+    sessionStorage.setItem('isLoggedIn', 'true');
+    sessionStorage.setItem('username', this.form.value.username ?? '');
+
+    this.router.navigateByUrl('/home');
+  }
+}
